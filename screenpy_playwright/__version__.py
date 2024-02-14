3cf98b3c5b51ad92690d61b55aa3f1ca776cea58
bandophahita <trenchrats@gmail.com>
2024-02-14 17:35:02 -0600

    sync __version__.py

--- screenpy_playwright/__version__.py
+++ screenpy_playwright/__version__.py
@@ -10,11 +10,15 @@ r"""
         '    `--' '  '   '   ' '   '  ' '  `-' '  '   '
 """
 
-__title__ = "screenpy_playwright"
-__description__ = "ScreenPy extension to enable interacting with Playwright."
-__url__ = "https://github.com/ScreenPyHQ/screenpy_playwright"
-__version__ = "0.0.1"
-__author__ = "Perry Goy"
-__author_email__ = "perry.goy@gmail.com"
-__license__ = "MIT"
-__copyright__ = "Copyright (c) 2022-2023 Perry Goy"
+import importlib.metadata as importlib_metadata
+
+metadata = importlib_metadata.metadata("screenpy_playwright")
+
+__title__ = metadata["Name"]
+__description__ = metadata["Summary"]
+__url__ = metadata["Home-page"]
+__version__ = metadata["Version"]
+__author__ = metadata["Author"]
+__author_email__ = metadata["Author-email"]
+__license__ = metadata["License"]
+__copyright__ = f"2019-2024 {__author__}"
